8c35740f4b3bce3ad4ddc98a5cd3fd43808ec824
csorz <csorz@qq.com>
2026-04-07 20:58:03 +0800

    feat: *

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,6 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import { setupSerialHandlers } from './handlers/serial'
-import { setupUsbHandlers } from './handlers/usb'
-import { setupBluetoothHandlers } from './handlers/bluetooth'
 
 function createWindow(): void {
   // Create the browser window.
@@ -33,7 +29,7 @@ function createWindow(): void {
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+  if (process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
@@ -43,13 +39,15 @@ function createWindow(): void {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
+  // Dynamic import for electron-toolkit/utils (requires app to be ready)
+  const { electronApp, optimizer, is } = await import('@electron-toolkit/utils')
+
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
@@ -57,7 +55,11 @@ app.whenReady().then(() => {
   // IPC test
   ipcMain.on('ping', () => console.log('pong'))
 
-  // Setup device handlers
+  // Setup device handlers (dynamic import)
+  const { setupSerialHandlers } = await import('./handlers/serial')
+  const { setupUsbHandlers } = await import('./handlers/usb')
+  const { setupBluetoothHandlers } = await import('./handlers/bluetooth')
+
   setupSerialHandlers()
   setupUsbHandlers()
   setupBluetoothHandlers()
@@ -79,6 +81,3 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
